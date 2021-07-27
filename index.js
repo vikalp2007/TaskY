264f33bf20f14f1d7b2b6259f418eaf2d9c8128b
vikalp2007 <vikalpchaturvedi02@gmail.com>
2021-07-27 21:20:10 +0530

    added delete button

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = [];
+let globalStore = [];
 
-const generateNewCard = (taskData) =>  `
+const generateNewCard = (taskData) => `
 <div class="col-md-6 col-lg-4">
       <div class="card">
         <div class="card-header d-flex justify-content-end gap-2">
@@ -25,19 +25,19 @@ const generateNewCard = (taskData) =>  `
 `;
 
 const loadInitialCardData = () => {
-  // access the local storage to get tasky card data
-  const getCardData = localStorage.getItem("tasky");
+    // access the local storage to get tasky card data
+    const getCardData = localStorage.getItem("tasky");
 
-  // convert to normal object
-  const {cards} = JSON.parse(getCardData);
+    // convert from string to normal object
+    const { cards } = JSON.parse(getCardData);
 
-  // loop over those array of task object to create HTML card and inject it to DOM
-  cards.map((cardObject) => {
-    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject))
+    // loop over those array of task object to create HTML card and inject it to DOM
+    cards.map((cardObject) => {
+        taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject))
 
-    // update our globalStore
-    globalStore.push(cardObject);
-  }) 
+        // update our globalStore
+        globalStore.push(cardObject);
+    })
 };
 
 const saveChanges = () => {
@@ -53,24 +53,24 @@ const saveChanges = () => {
 
     globalStore.push(taskData);
 
-    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+    localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
 };
 
 const deleteCard = (event) => {
-  event = window.event;
-  // giving id 
-  const targetID = event.target.id;
-  const tagname = event.target.tagName; //BUTTON
+    event = window.event; // gives browser related properties & our HTML element that we interacted with
+    // giving id 
+    const targetID = event.target.id;
+    const tagname = event.target.tagName; //BUTTON
     // match the id of the element with the id inside the globalStore
     // if match found, remove
 
-  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
-  localStorage.setItem("tasky", JSON.stringify({cards:globalStore})); 
-  // contact parent
-  
-  if(tagname === "BUTTON"){
-    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
-  }else{
-    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
-  }
+    globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+    localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+    // contact parent
+
+    if (tagname === "BUTTON") {
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    } else {
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
 };
